4cee03bfc28eecf1d9f673276ab036985b262636
Chad Wallace <cewall0@gmail.com>
2024-01-01 22:14:29 -0500

    Initial Commit

---
+++ Sluff Scorecard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sluff Scorecard
+//
+//  Created by Chad Wallace on 1/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sluff Scorecard/Sluff_ScorecardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sluff_ScorecardApp.swift
+//  Sluff Scorecard
+//
+//  Created by Chad Wallace on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Sluff_ScorecardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
