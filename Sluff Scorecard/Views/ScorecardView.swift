bad5b5d6f8fce3b47d90a51e824a6c265776b666
Chad Wallace <cewall0@gmail.com>
2024-02-16 15:31:12 -0500

    ZStack on scorecardview so I can toggle on/off a numberofplayers View that I still need to create.

--- Sluff Scorecard/Views/ScorecardView.swift
+++ Sluff Scorecard/Views/ScorecardView.swift
@@ -20,70 +20,73 @@ struct ScorecardView: View {
         
         NavigationStack(path: $router.path) {
             
-            VStack{
+            ZStack{
                 
-                TitleView()
-                
-                TeamNameView()
-                
-                TeamScoresView()
-                
-                VStack {
-                    GeometryReader{geo in
-                        List {
-                            ForEach(game.playersList.indices, id: \.self) { index in
-                                HStack {
-                                    Text("D")
-                                        .frame(width: CGFloat(geo.size.width*0.05))
-                                        .fontWeight(.bold)
-                                        .padding(.leading, 40.0)
-                                        .foregroundColor(game.playersList[index].isDealer ? .yellow.opacity(1.0): .yellow.opacity(0.0))
+                VStack{
+                    
+                    TitleView()
+                    
+                    TeamNameView()
+                    
+                    TeamScoresView()
+                    
+                    VStack {
+                        GeometryReader{geo in
+                            List {
+                                ForEach(game.playersList.indices, id: \.self) { index in
+                                    HStack {
+                                        Text("D")
+                                            .frame(width: CGFloat(geo.size.width*0.05))
+                                            .fontWeight(.bold)
+                                            .padding(.leading, 40.0)
+                                            .foregroundColor(game.playersList[index].isDealer ? .yellow.opacity(1.0): .yellow.opacity(0.0))
+                                        
+                                        PlayerNameView(game: game, playerIndex: index)
+                                            .frame(width: CGFloat(geo.size.width*0.5))
+                                        
+                                        BidPickerView(game: game, playerIndex: index, onBidChanged: {
+                                            game.setTeamBids(from: game.playersList)
+                                            game.checkAllBid()
+                                        }
+                                        )
+                                        .frame(width: CGFloat(geo.size.width*0.30))
+                                        .padding(.trailing, 50.0)
+                                    } // end HStack
+                                    .frame(width: CGFloat(geo.size.width*0.95))
+                                    .foregroundColor(.black.opacity(1.0))
+                                    .tint(.black)
+                                    .background(index % 2 == 0 ? .teal.opacity(0.3) : .gray.opacity(0.3))
+                                    .cornerRadius(8)
                                     
-                                    PlayerNameView(game: game, playerIndex: index)
-                                        .frame(width: CGFloat(geo.size.width*0.5))
                                     
-                                    BidPickerView(game: game, playerIndex: index, onBidChanged: {
-                                        game.setTeamBids(from: game.playersList)
-                                        game.checkAllBid()
-                                    }
-                                    )
-                                    .frame(width: CGFloat(geo.size.width*0.30))
-                                    .padding(.trailing, 50.0)
-                                } // end HStack
-                                .frame(width: CGFloat(geo.size.width*0.95))
-                                .foregroundColor(.black.opacity(1.0))
-                                .tint(.black)
-                                .background(index % 2 == 0 ? .teal.opacity(0.3) : .gray.opacity(0.3))
-                                .cornerRadius(8)
-                                
-                                
-                            }
-                        }// end List
-                        .listStyle(.plain)
+                                }
+                            }// end List
+                            .listStyle(.plain)
+                            
+                        }
                         
-                    }
-                    
-                    Button(action: {
-                        game.resetTricksSluffsWon()
-                        router.path.append(1) // case 1 sends to ScoreHandView() See NavigationDestination below
-                    }) {
-                        Text("Score this hand")
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .disabled(game.notAllBid)
-                   
-                    Spacer()
-                    
-                } .navigationDestination(for: Int.self) { destination in
-                    switch destination {
-                    case 1:
-                        ScoreHandView().environmentObject(router)
-                    case 2:
-                        SettingsView().environmentObject(router)
-                    case 3:
-                        WinnerView().environmentObject(router)
-                    default:
-                        EmptyView()
+                        Button(action: {
+                            game.resetTricksSluffsWon()
+                            router.path.append(1) // case 1 sends to ScoreHandView() See NavigationDestination below
+                        }) {
+                            Text("Score this hand")
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .disabled(game.notAllBid)
+                        
+                        Spacer()
+                        
+                    } .navigationDestination(for: Int.self) { destination in
+                        switch destination {
+                        case 1:
+                            ScoreHandView().environmentObject(router)
+                        case 2:
+                            SettingsView().environmentObject(router)
+                        case 3:
+                            WinnerView().environmentObject(router)
+                        default:
+                            EmptyView()
+                        }
                     }
                 }
             } // end NavigationStack
