e259d57dc2d448e3ee3802033edb82361ae83e08
Chad Wallace <cewall0@gmail.com>
2024-03-07 14:25:16 -0500

    Added History View to track points round by round

---
+++ Sluff Scorecard/Models/RunningScores.swift
@@ -0,0 +1,8 @@
+//
+//  RunningScores.swift
+//  Sluff Scorecard
+//
+//  Created by Chad Wallace on 3/7/24.
+//
+
+import Foundation
---
+++ Sluff Scorecard/Views/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  Sluff Scorecard
+//
+//  Created by Chad Wallace on 3/7/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
